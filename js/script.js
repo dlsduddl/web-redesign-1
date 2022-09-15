35b1488657db764de51452f26bc878fce90f66c9
dlsduddl <inyoung106@naver.com>
2022-09-15 23:01:39 +0900

    20220915 배너 수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 console.clear();
-console.clear();
 
 // 메뉴바 호버시 컬러변경
 $(".menu-bar > nav > ul").mouseenter(function(){
@@ -8,3 +7,25 @@ $(".menu-bar > nav > ul").mouseenter(function(){
 $(".menu-bar > nav > ul").mouseleave(function(){
   $(".top-bar").removeClass("active");
 });
+
+
+// 배너 스와이퍼
+function SliderBox1__init() {
+  const swiper = new Swiper(".slider-box-1 .swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-1 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-1 .swiper-button-next",
+      prevEl: ".slider-box-1 .swiper-button-prev"
+    }
+  });
+}
+
+SliderBox1__init();
