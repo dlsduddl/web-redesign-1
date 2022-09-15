dd5b0b5fefdc05089db7a5ec9076a25ca5bb6c42
dlsduddl <inyoung106@naver.com>
2022-09-15 20:45:48 +0900

    20220915 헤더 마크업 완료

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.clear();
+console.clear();
+
+// 메뉴바 호버시 컬러변경
+$(".menu-bar > nav > ul").mouseenter(function(){
+  $(".top-bar").addClass("active");
+});
+$(".menu-bar > nav > ul").mouseleave(function(){
+  $(".top-bar").removeClass("active");
+});
