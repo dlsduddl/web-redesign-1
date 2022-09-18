627078e82ca46830f9d24c330b0e56786c93df2c
dlsduddl <inyoung106@naver.com>
2022-09-18 23:29:58 +0900

    20220918 섹션2 마크업

--- js/script.js
+++ js/script.js
@@ -29,3 +29,43 @@ function SliderBox1__init() {
 }
 
 SliderBox1__init();
+
+function SliderBox2__init() {
+  const swiper = new Swiper(".slider-box-2 .swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-2 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-2 .swiper-button-next",
+      prevEl: ".slider-box-2 .swiper-button-prev"
+    }
+  });
+}
+
+SliderBox2__init();
+
+function SliderBox3__init() {
+  const swiper = new Swiper(".slider-box-3 .swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-3 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-3 .swiper-button-next",
+      prevEl: ".slider-box-3 .swiper-button-prev"
+    }
+  });
+}
+
+SliderBox3__init();
